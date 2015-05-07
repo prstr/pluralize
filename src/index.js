3b0be964278d2a01d3c8fb55d60912584aabdf15
Boris Okunskiy <i@savant.pro>
2015-05-07 17:56:12 +0300

    Added eslint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-"use strict";
+'use strict';
 
-module.exports = exports = function(num, s1, s2, s5) {
+module.exports = exports = function (num, s1, s2, s5) {
   function p(a) {
     if (a % 10 == 1 && a % 100 != 11) return 0;
     else if (a % 10 >= 2 && a % 10 <= 4 && ( a % 100 < 10 || a % 100 >= 20)) return 1;
     else return 2;
   }
+
   switch (p(num)) {
     case 0:
       return s1.replace('%s', num);
@@ -16,8 +17,8 @@ module.exports = exports = function(num, s1, s2, s5) {
   }
 };
 
-exports.formatter = function(s1, s2, s5) {
-  return function(num) {
+exports.formatter = function (s1, s2, s5) {
+  return function (num) {
     return exports(num, s1, s2, s5);
-  }
+  };
 };
--- test/index.js
+++ test/index.js
@@ -1,23 +1,23 @@
-"use strict";
+'use strict';
 
 var assert = require('assert')
   , pluralize = require('../src/index');
 
-describe('Pluralize', function() {
+describe('Pluralize', function () {
 
-  it('should emit correct "one"', function() {
+  it('should emit correct "one"', function () {
     assert(pluralize(1, '%s корова', '%s коровы', '%s коров'), '1 корова');
   });
 
-  it('should emit correct "few"', function() {
+  it('should emit correct "few"', function () {
     assert(pluralize(3, '%s корова', '%s коровы', '%s коров'), '3 коровы');
   });
 
-  it('should emit correct "many"', function() {
+  it('should emit correct "many"', function () {
     assert(pluralize(5, '%s корова', '%s коровы', '%s коров'), '5 коров');
   });
 
-  it('should work with formatter', function() {
+  it('should work with formatter', function () {
     var cows = pluralize.formatter('%s корова', '%s коровы', '%s коров');
     assert(cows(100), '100 коров');
     assert(cows(101), '101 корова');
